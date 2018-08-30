ad53933f545c7b6e096844f0920f79b020481497
sbha <sbharty@gmail.com>
2018-08-30 15:18:52 -0500

    abbreviation or long title option

--- R/clean_streets.R
+++ R/clean_streets.R
@@ -10,8 +10,9 @@
 #'
 #' @export street_cleaner
 
-street_cleaner <- function(x){
+street_cleaner <- function(x, abbr = FALSE){
   library(stringr)
+  
   df_key <- read.table(text = '
                        title abbreviation
                        ROAD RD
@@ -32,15 +33,23 @@ street_cleaner <- function(x){
                        SOUTHEAST SE
                        NORTHEAST NE
                        NORTHWEST NW', header = TRUE, stringsAsFactors = FALSE)
-
-  replace_with <- c(unlist(df_key$title))
-  to_replace <- c(unlist(df_key$abbreviation))
+  
+  if (abbr == FALSE) {
+    to_replace <- c(unlist(df_key$abbreviation))
+    replace_with <- c(unlist(df_key$title))
+  } else if (abbr == TRUE){
+    to_replace <- c(unlist(df_key$title))
+    replace_with <- c(unlist(df_key$abbreviation))
+  }
+  
+  
+  #replace_with <- paste0('\\b', replace_with, '\\b')
   to_replace <- paste0('\\b', to_replace, '\\b')
   names(replace_with) <- to_replace
-
+  
   address <- gsub('\\.|,', ' ', toupper(x))
   address <- gsub('\\s+', ' ', address)
-
+  
   str_out <- str_replace_all(address, replace_with)
   trimws(str_out)
 }
--- README.md
+++ README.md
@@ -10,7 +10,13 @@ devtools::install_github("sbha/cleanstreets")
 
 ## Usage
 ```r
+# The default if to go from abbreviations to long names:
 library(cleanstreets)
 street_cleaner('123 Main St.')
+#> "123 MAIN STREET"
+
+# Setting the argument abbr = TRUE converts longer names to abbreviations:
+street_cleaner('123 Main Street', abbr = TRUE)
+#> "123 MAIN ST"
 
 ```
